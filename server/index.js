340ff0c5376eee5e0a16051817557933a8cdcef3
sanket yelugotla <120624203+sanketyelugotla@users.noreply.github.com>
2025-12-30 14:38:02 +0530

    limit

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Request logging middleware
 app.use((req, res, next) => {
